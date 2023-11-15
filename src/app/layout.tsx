5a2687a47f4390419869a92421fba584307e3595
hwld <0701hw@gmail.com>
2023-11-15 13:05:33 +0900

    chore: prettierの設定

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,21 +2,15 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
-
 export const metadata: Metadata = {
   title: "Zero one pages",
   description: "page layouts",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ja">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
+import clsx from "clsx";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
 export default function Home() {
   return (
-    <div className="bg-neutral-800 h-[100dvh] flex justify-center items-center">
-      <div className="flex flex-col gap-6 text-neutral-300 text-center">
+    <div
+      className={clsx(
+        inter.className,
+        "flex h-[100dvh] items-center justify-center bg-neutral-800",
+      )}
+    >
+      <div className="flex flex-col gap-6 text-center text-neutral-300">
         <h1 className="max-w-2xl text-7xl font-bold">Zero one pages</h1>
         <p className="text-lg">0から作った画面をまとめる</p>
       </div>
