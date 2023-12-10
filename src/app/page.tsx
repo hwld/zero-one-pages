d9c06fdb4cb1e6d2b145b755e33cc245f3b25b5a
hwld <0701hw@gmail.com>
2023-12-10 13:23:57 +0900

    change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,32 +10,49 @@ export default function Home() {
     <div
       className={clsx(
         inter.className,
-        "flex h-[100dvh] flex-col items-center justify-center gap-10 bg-zinc-900",
+        "flex h-[100dvh] flex-col items-center overflow-auto bg-zinc-900 pt-[200px]",
       )}
     >
-      <div className="flex flex-col gap-3 text-center text-zinc-200">
+      <div className="flex flex-col gap-2 text-center text-zinc-200">
         <h1 className="text-3xl font-bold">Zero one pages</h1>
-        <p className="text-sm">0から作った画面をまとめる</p>
+        <p className="text-sm text-zinc-400">0から作った画面をまとめる</p>
       </div>
-      {pages.map((p, i) => {
-        return <PageCard page={p} key={p.href} number={i + 1} />;
-      })}
+      <div className="mt-[80px] flex max-w-[950px] flex-wrap justify-center gap-5">
+        {pages.map((p, i) => {
+          return <PageCard page={p} key={p.href} number={i + 1} />;
+        })}
+        {[...new Array(9 - pages.length)].map((i) => {
+          return <PageCard key={i} dummy />;
+        })}
+      </div>
     </div>
   );
 }
 
-const PageCard: React.FC<{ page: Page; number: number }> = ({ page, number }) => {
+type Props = { page: Page; number: number; dummy?: false } | { dummy: true };
+const PageCard: React.FC<Props> = (props) => {
+  const rootClass =
+    "flex min-h-[120px] w-[300px] flex-col rounded-lg border-2 border-zinc-800 bg-zinc-700 p-4 text-zinc-200 outline outline-2 outline-zinc-700 transition-colors";
+
+  if (props.dummy) {
+    return <div className={rootClass} />;
+  }
+
   return (
     <Link
-      href={page.href}
-      className="flex w-[300px] flex-col gap-3 rounded-lg border border-zinc-500 p-5 text-zinc-200 hover:bg-white/5"
+      href={props.page.href}
+      className={clsx(rootClass, "hover:bg-zinc-600")}
     >
-      <div className="flex items-center gap-2">
-        <div className="text-lg font-bold">{number}.</div>
-        <div>{page.title}</div>
-      </div>
-      <div className="h-[1px] bg-zinc-600" />
-      <p>{page.description}</p>
+      {!props.dummy && (
+        <>
+          <div className="flex items-end gap-2">
+            <div className="text-xl font-bold">{props.number}.</div>
+            <div>{props.page.title}</div>
+          </div>
+          <div className="mb-4 mt-2 h-[1px] bg-zinc-400" />
+          <p className="text-sm">{props.page.description}</p>
+        </>
+      )}
     </Link>
   );
 };
--- src/app/pages.ts
+++ src/app/pages.ts
@@ -4,6 +4,7 @@ export const pages: Page[] = [
   {
     href: "/1",
     title: "todoリスト",
-    description: "画面下にあるinput周りのコンポーネントが好き。結構時間かかった。",
+    description:
+      "画面下にあるinput周りのコンポーネントが好き。結構時間かかった。",
   },
 ];
