9fd6cd1df947c04fef6e1e9e1cdb74dadd777ec3
hwld <0701hw@gmail.com>
2024-02-27 04:04:55 +0900

    ディレクトリ名を変更

--- src/app/pages.ts
+++ src/app/pages.ts
@@ -8,34 +8,34 @@ export type Page = {
 
 export const pages: Page[] = [
   {
-    href: "/1",
+    href: "/todo-1",
     title: "todoリスト",
     description:
       "浮いてるinputを使ったTodoリスト。\ninputの隣のメニューがお気に入り。",
     tags: ["PC", "MOBILE", "LAYOUT"],
   },
   {
-    href: "/2",
+    href: "/todo-2",
     title: "todoリスト2",
     description:
       "表形式のtodoリスト。\nテーブルの部分だけカードになっているのがお気に入り。",
     tags: ["PC", "LAYOUT"],
   },
   {
-    href: "/3",
+    href: "/dynamic-menu",
     title: "変形するメニュー",
     description: "youtubeの設定メニューを見て作りたくなった。\n🥱🥱🥱😀😀😀",
     tags: ["PC", "MOBILE", "PART"],
   },
   {
-    href: "/4",
+    href: "/chat",
     title: "チャット",
     description:
       "DiscordみたいなUI。\n設定ページを初めて作ったが、項目が多いとレイアウトが大変そうだなぁと思った。",
     tags: ["PC", "LAYOUT"],
   },
   {
-    href: "/5",
+    href: "/dynamic-island",
     title: "変形するメニュー2",
     description:
       "Dynamic Islandみたいなメニュー。\nframer-motionでspring animationを使ってみた。",
