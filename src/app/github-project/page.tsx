f397218d571ad4aa13fc4501812a9452c741ffc6
hwld <0701hw@gmail.com>
2024-03-28 01:04:41 +0900

    kanbanを実装

--- src/app/github-project/page.tsx
+++ src/app/github-project/page.tsx
@@ -2,11 +2,13 @@ import clsx from "clsx";
 import {
   ChevronDown,
   ChevronDownIcon,
+  CircleDashedIcon,
   CircleDotIcon,
   GitPullRequestIcon,
   InboxIcon,
   KanbanSquareIcon,
   LineChartIcon,
+  ListFilterIcon,
   LucideIcon,
   MenuIcon,
   MoreHorizontalIcon,
@@ -17,6 +19,45 @@ import {
 } from "lucide-react";
 import React from "react";
 import { ReactNode } from "react";
+
+type Kanban = {
+  color: StatusIconColor;
+  status: "Todo" | "In Progress" | "Done" | "Archive" | "On Hold";
+  count: number;
+  description: string;
+};
+const kanbans: Kanban[] = [
+  {
+    color: "green",
+    status: "Todo",
+    count: 10,
+    description: "This item hasn't been started",
+  },
+  {
+    color: "orange",
+    status: "In Progress",
+    count: 5,
+    description: "This is actively being worked on",
+  },
+  {
+    color: "red",
+    status: "On Hold",
+    count: 3,
+    description: "This item is on hold",
+  },
+  {
+    color: "purple",
+    status: "Done",
+    count: 0,
+    description: "This has been completed",
+  },
+  {
+    color: "gray",
+    status: "Archive",
+    count: 3,
+    description: "This item has been archived",
+  },
+];
 
 const GitHubProjectPage: React.FC = () => {
   return (
@@ -44,9 +85,9 @@ const GitHubProjectPage: React.FC = () => {
           <Tab>Kanban2</Tab>
           <Tab icon={PlusIcon}>New view</Tab>
         </div>
-        <div className="flex bg-neutral-800">
+        <div className="flex w-[100dvw] bg-neutral-800">
           <SlicerPanel />
-          <div className="p-4">main</div>
+          <MainPanel />
         </div>
       </div>
     </div>
@@ -54,39 +95,117 @@ const GitHubProjectPage: React.FC = () => {
 };
 export default GitHubProjectPage;
 
+const MainPanel: React.FC = () => {
+  return (
+    <div className="flex min-w-0 grow flex-col">
+      <div className="flex items-center gap-4 p-4">
+        <div className="flex h-8 grow items-center rounded-md border border-neutral-600 bg-transparent pl-2 focus-within:border-blue-500">
+          <ListFilterIcon size={16} className="text-neutral-400" />
+          <input
+            className="h-full grow bg-transparent px-2 text-sm placeholder:text-neutral-400 focus-visible:outline-none"
+            placeholder="Filter by keyword or by field"
+          />
+        </div>
+        <div className="flex gap-2">
+          <Button>Discard</Button>
+          <Button color="primary">Save</Button>
+        </div>
+      </div>
+      <div className="flex grow gap-4 overflow-x-scroll px-4 py-2">
+        {kanbans.map((kanban, i) => {
+          return <Kanban key={i} {...kanban} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+const Button: React.FC<{
+  color?: "primary" | "default";
+  children: ReactNode;
+}> = ({ color = "default", children }) => {
+  const colorClass = {
+    default:
+      "bg-neutral-800 border border-neutral-700 text-neutral-200 hover:bg-neutral-700",
+    primary: "bg-green-700 border border-green-600 hover:bg-green-600",
+  };
+
+  return (
+    <button
+      className={clsx(
+        "h-7 rounded-md px-2 text-xs transition-colors",
+        colorClass[color],
+      )}
+    >
+      {children}
+    </button>
+  );
+};
+
+const Kanban: React.FC<{
+  color: StatusIconColor;
+  status: string;
+  count: number;
+  description: string;
+}> = ({ color, status, count, description }) => {
+  return (
+    <div className="flex h-full w-[350px] shrink-0 flex-col rounded-md border border-neutral-700 bg-neutral-900">
+      <div className="flex items-center justify-between px-4 pb-2 pt-4">
+        <div className="flex items-center gap-2">
+          <StatusIcon color={color} />
+          <div className="font-bold">{status}</div>
+          <CountBadge count={count} />
+        </div>
+        <div className="flex items-center">
+          <button className="grid size-6 place-items-center rounded-md text-neutral-400 transition-colors hover:bg-white/15">
+            <MoreHorizontalIcon size={20} />
+          </button>
+        </div>
+      </div>
+      <div className="px-4 pb-2 text-sm text-neutral-400">{description}</div>
+      <div className="flex grow flex-col gap-2 overflow-auto scroll-auto p-2">
+        {[...new Array(count)].map((_, i) => {
+          return <KanbanItem key={i} />;
+        })}
+      </div>
+      <div className="grid place-items-center p-1">
+        <button className="flex w-full items-center gap-1 rounded-md px-4 py-2 text-neutral-300 transition-colors hover:bg-neutral-800">
+          <PlusIcon size={16} />
+          <span className="text-sm">Add Item</span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+const KanbanItem: React.FC = () => {
+  return (
+    <div className="group flex cursor-pointer flex-col gap-1 rounded-md border border-neutral-700 bg-neutral-800 p-2 transition-colors hover:border-neutral-600">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-1 text-neutral-400">
+          <CircleDashedIcon size={16} strokeWidth={3} />
+          <div className="text-xs">Draft</div>
+          <button className="grid size-5 place-items-center rounded opacity-0 transition-colors hover:bg-white/15 group-hover:opacity-100">
+            <MoreHorizontalIcon size={18} />
+          </button>
+        </div>
+      </div>
+      <div className="text-sm">task title</div>
+    </div>
+  );
+};
+
 const SlicerPanel: React.FC = () => {
   return (
-    <div className="flex w-[350px] flex-col gap-2 overflow-auto border-r border-neutral-600 p-4">
+    <div className="flex w-[350px] shrink-0 flex-col gap-2 overflow-auto border-r border-neutral-600 p-4">
       <button className="flex h-8 w-fit items-center gap-1 rounded-md px-2 text-sm hover:bg-white/10">
         <span>Status</span>
         <ChevronDownIcon size={16} />
       </button>
       <ul className="w-full [&>li:last-child]:border-b-0">
-        <SlicerListItem
-          color="green"
-          label="Todo"
-          description="This item hasn't been started"
-          count={10}
-        />
-        <SlicerListItem
-          active
-          color="orange"
-          label="In Progress"
-          description="This is actively being worked on"
-          count={5}
-        />
-        <SlicerListItem
-          color="purple"
-          label="Done"
-          description="This has been completed"
-          count={0}
-        />
-        <SlicerListItem
-          color="gray"
-          label="Archive"
-          description="This item has been archived"
-          count={3}
-        />
+        {kanbans.map((kanban, i) => {
+          return <SlicerListItem key={i} {...kanban} label={kanban.status} />;
+        })}
         <SlicerListItem label="No status" count={100} />
       </ul>
     </div>
@@ -95,19 +214,11 @@ const SlicerPanel: React.FC = () => {
 
 const SlicerListItem: React.FC<{
   active?: boolean;
-  color?: "green" | "orange" | "purple" | "gray" | "transparent";
+  color?: StatusIconColor;
   label: string;
   description?: string;
   count: number;
 }> = ({ active = false, color = "transparent", label, description, count }) => {
-  const iconClass = {
-    green: "bg-green-600/20 border-green-600",
-    orange: "bg-orange-600/20 border-orange-600",
-    purple: "bg-purple-600/20 border-purple-600",
-    gray: "bg-neutral-500/20 border-neutral-500",
-    transparent: "bg-transparent border-transparent",
-  };
-
   return (
     <li
       className={clsx(
@@ -123,9 +234,7 @@ const SlicerListItem: React.FC<{
         )}
       >
         <div className="flex items-start gap-2">
-          <div
-            className={clsx("size-4 rounded-full border-2", iconClass[color])}
-          />
+          <StatusIcon color={color} />
           <div className="flex flex-col items-start">
             <div className="text-sm">{label}</div>
             {description && (
@@ -133,11 +242,44 @@ const SlicerListItem: React.FC<{
             )}
           </div>
         </div>
-        <div className="min-w-6 rounded-full bg-white/10 p-1 text-xs text-neutral-400">
-          {count}
-        </div>
+        <CountBadge count={count} />
       </button>
     </li>
+  );
+};
+
+type StatusIconColor =
+  | "green"
+  | "orange"
+  | "red"
+  | "purple"
+  | "gray"
+  | "transparent";
+const StatusIcon: React.FC<{ color: StatusIconColor }> = ({ color }) => {
+  const iconClass = {
+    green: "bg-green-600/20 border-green-600",
+    orange: "bg-orange-600/20 border-orange-600",
+    red: "bg-red-600/20 border-red-600",
+    purple: "bg-purple-600/20 border-purple-600",
+    gray: "bg-neutral-500/20 border-neutral-500",
+    transparent: "bg-transparent border-transparent",
+  };
+
+  return (
+    <div
+      className={clsx(
+        "size-4 shrink-0 rounded-full border-2",
+        iconClass[color],
+      )}
+    />
+  );
+};
+
+const CountBadge: React.FC<{ count: number }> = ({ count }) => {
+  return (
+    <div className="grid min-w-6 place-items-center rounded-full bg-white/10 p-1 text-xs text-neutral-400">
+      {count}
+    </div>
   );
 };
 
@@ -197,7 +339,7 @@ const Header: React.FC = () => {
     <div className="flex items-center justify-between gap-2 border-b border-neutral-600 px-4">
       <div className="flex items-center gap-4">
         <HeaderButton icon={MenuIcon} />
-        <div className="size-8 rounded-full bg-neutral-300" />
+        <div className="size-8 shrink-0 rounded-full bg-neutral-300" />
         <div className="flex items-center">
           <BreadCrumbItem>hwld</BreadCrumbItem>
           <BreadCrumbSeparator />
@@ -245,7 +387,7 @@ const BreadCrumbItem: React.FC<{ children: ReactNode; active?: boolean }> = ({
   return (
     <button
       className={clsx(
-        "flex h-6 cursor-pointer items-center rounded-md px-1 text-sm transition-colors hover:bg-white/15",
+        "flex h-6 cursor-pointer items-center text-nowrap rounded-md px-1 text-sm transition-colors hover:bg-white/15",
         active && "font-bold",
       )}
     >
@@ -261,7 +403,7 @@ const HeaderButton: React.FC<{ icon: LucideIcon; rightIcon?: LucideIcon }> = ({
   return (
     <button
       className={clsx(
-        "flex h-8 cursor-pointer items-center justify-center gap-1 rounded-md border border-neutral-600 text-neutral-400 transition-colors hover:bg-white/10",
+        "flex h-8 shrink-0 cursor-pointer items-center justify-center gap-1 rounded-md border border-neutral-600 text-neutral-400 transition-colors hover:bg-white/10",
         RightIcon ? "px-2" : "w-8",
       )}
     >
