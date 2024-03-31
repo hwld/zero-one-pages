a5ce59de6286aa2606cae72c004311af78f6c078
hwld <0701hw@gmail.com>
2024-04-01 02:27:49 +0900

    user-drawer実装

--- src/app/github-project/_components/drawer/item.tsx
+++ src/app/github-project/_components/drawer/item.tsx
@@ -1,17 +1,22 @@
-import { ComponentPropsWithoutRef } from "react";
+import { ComponentPropsWithoutRef, ReactNode } from "react";
 
 type DrawerItemProps = {
-  icon: React.FC<{ size?: number | string | undefined; className?: string }>;
+  icon?: React.FC<{ size?: number | string | undefined; className?: string }>;
+  left?: ReactNode;
   title: string;
 } & ComponentPropsWithoutRef<"button">;
 export const DrawerItem: React.FC<DrawerItemProps> = ({
   icon: Icon,
+  left,
   title,
 }) => {
   return (
-    <button className="flex h-8 w-full items-center gap-2 rounded-md px-2 text-neutral-100 transition-colors hover:bg-white/15">
-      <Icon size={16} className="text-neutral-400" />
-      <span className="text-sm">{title}</span>
+    <button className="flex h-8 w-full items-center justify-between gap-2 rounded-md px-2 text-neutral-100 transition-colors hover:bg-white/15">
+      <div className="flex gap-2">
+        {Icon && <Icon size={16} className="text-neutral-400" />}
+        <span className="text-sm">{title}</span>
+      </div>
+      {left}
     </button>
   );
 };
--- src/app/github-project/_components/user-drawer/user-drawer.tsx
+++ src/app/github-project/_components/user-drawer/user-drawer.tsx
@@ -1,6 +1,27 @@
 import { useState } from "react";
 import { Drawer } from "../drawer/drawer";
 import { Avatar } from "../avatar";
+import { CloseButton } from "../close-button";
+import { DrawerItem } from "../drawer/item";
+import {
+  BookMarkedIcon,
+  BuildingIcon,
+  CodeSquareIcon,
+  ComputerIcon,
+  FlaskConicalIcon,
+  GlobeIcon,
+  HeartIcon,
+  KanbanSquareIcon,
+  MessagesSquareIcon,
+  SettingsIcon,
+  SmileIcon,
+  StarIcon,
+  UploadIcon,
+  UserIcon,
+  UserPlusIcon,
+  UsersIcon,
+} from "lucide-react";
+import { Divider } from "../divider";
 
 export const UserDrawer: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -16,7 +37,63 @@ export const UserDrawer: React.FC = () => {
         </button>
       }
     >
-      <div></div>
+      <div className="p-4">
+        <div className="flex items-center justify-between gap-2">
+          <div className="flex items-center gap-2">
+            <Avatar />
+            <div>
+              <div className="text-sm font-bold">hwld</div>
+              <div className="text-sm text-neutral-400">hwld</div>
+            </div>
+          </div>
+          <div className="self-start">
+            <CloseButton onClick={() => setIsOpen(false)} />
+          </div>
+        </div>
+        <div className="mt-4 space-y-2">
+          <DrawerItem icon={SmileIcon} title="Set status" />
+          <Divider />
+          <div>
+            <DrawerItem icon={UserIcon} title="Your profile" />
+            <DrawerItem icon={UserPlusIcon} title="Add account" />
+          </div>
+          <Divider />
+          <div>
+            <DrawerItem icon={BookMarkedIcon} title="Your repositories" />
+            <DrawerItem icon={KanbanSquareIcon} title="Your projects" />
+            <DrawerItem icon={ComputerIcon} title="Your Copilot" />
+            <DrawerItem icon={BuildingIcon} title="Your organizations" />
+            <DrawerItem icon={GlobeIcon} title="Your enterprises" />
+            <DrawerItem icon={StarIcon} title="Your stars" />
+            <DrawerItem icon={HeartIcon} title="Your sponsors" />
+            <DrawerItem icon={CodeSquareIcon} title="Your gists" />
+          </div>
+          <Divider />
+          <div>
+            <DrawerItem icon={UploadIcon} title="Upgrade" />
+            <DrawerItem
+              icon={GlobeIcon}
+              title="Try Enterprise"
+              left={
+                <span className="flex h-5 items-center rounded-full border border-neutral-400 px-2 text-xs">
+                  Free
+                </span>
+              }
+            />
+            <DrawerItem icon={FlaskConicalIcon} title="Feature preview" />
+            <DrawerItem icon={SettingsIcon} title="Settings" />
+          </div>
+          <Divider />
+          <div>
+            <DrawerItem icon={UsersIcon} title="GitHub Support" />
+            <DrawerItem icon={MessagesSquareIcon} title="GitHub Community" />
+          </div>
+          <Divider />
+          <div>
+            <DrawerItem title="Sign out" />
+          </div>
+        </div>
+      </div>
     </Drawer>
   );
 };
