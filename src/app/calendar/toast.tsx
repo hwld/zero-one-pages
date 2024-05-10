60071b5c11b2dd6986775fd7de47ae8eeaee7a9e
hwld <0701hw@gmail.com>
2024-05-10 19:59:49 +0900

    toastを実装

---
+++ src/app/calendar/toast.tsx
@@ -0,0 +1,139 @@
+import {
+  ComponentPropsWithoutRef,
+  Dispatch,
+  PropsWithChildren,
+  SetStateAction,
+  createContext,
+  useCallback,
+  useContext,
+  useState,
+} from "react";
+import * as RxToast from "@radix-ui/react-toast";
+import { AnimatePresence, motion } from "framer-motion";
+import { TbInfoCircle, TbX } from "react-icons/tb";
+
+type Toast = { id: string; title: string } & (
+  | { action?: undefined }
+  | { action: (params: { close: () => void }) => void; actionText: string }
+);
+// 普通にOmitを使うとkeyofで共有のプロパティしか返さないのでunion distributionを使ってすべてのkeyをリストアップできるようにする
+type CreateToastInput<T = Toast> = T extends unknown ? Omit<T, "id"> : never;
+
+type ToastContext = [Toast[], Dispatch<SetStateAction<Toast[]>>];
+const ToastContext = createContext<ToastContext | undefined>(undefined);
+
+export const ToastProvider: React.FC<PropsWithChildren> = ({ children }) => {
+  const toastState = useState<Toast[]>([]);
+  const [toasts, setToasts] = toastState;
+
+  const closeToast = (id: string) => {
+    setToasts((toasts) => toasts.filter((t) => t.id !== id));
+  };
+
+  const handleOpenChange = (open: boolean, toast: Toast) => {
+    if (!open) {
+      closeToast(toast.id);
+    }
+  };
+
+  const handleClickAction = (toast: Toast) => {
+    toast.action?.({ close: () => closeToast(toast.id) });
+  };
+
+  return (
+    <ToastContext.Provider value={toastState}>
+      <RxToast.Provider>
+        {children}
+        <AnimatePresence>
+          {toasts.map((toast) => {
+            return (
+              <RxToast.Root
+                key={toast.id}
+                asChild
+                onOpenChange={(o) => handleOpenChange(o, toast)}
+                onSwipeStart={(e) => e.preventDefault()}
+                onSwipeMove={(e) => e.preventDefault()}
+                onSwipeCancel={(e) => e.preventDefault()}
+                onSwipeEnd={(e) => e.preventDefault()}
+                forceMount
+              >
+                <motion.div
+                  layout
+                  className="relative flex w-[300px] flex-col gap-2 rounded border border-neutral-300 bg-neutral-100 p-2 text-neutral-700 shadow"
+                  initial={{ opacity: 0, y: 8 }}
+                  animate={{ opacity: 1, scale: 1, y: 0 }}
+                  exit={{ opacity: 0, y: 8, transition: { duration: 0.1 } }}
+                >
+                  <div className="flex justify-between">
+                    <div className="flex items-center gap-2 text-sm">
+                      <TbInfoCircle size={16} />
+                      {toast.title}
+                    </div>
+                    <button
+                      className="rounde grid size-6 place-items-center rounded transition-colors hover:bg-black/10"
+                      onClick={() => {
+                        closeToast(toast.id);
+                      }}
+                    >
+                      <TbX />
+                    </button>
+                  </div>
+                  {toast.action && (
+                    <div className="flex w-full justify-end">
+                      <ToastAction onClick={() => handleClickAction(toast)}>
+                        {toast.actionText}
+                      </ToastAction>
+                    </div>
+                  )}
+                </motion.div>
+              </RxToast.Root>
+            );
+          })}
+        </AnimatePresence>
+        <RxToast.Viewport className="fixed bottom-4 right-4 z-[100] flex flex-col gap-1" />
+      </RxToast.Provider>
+    </ToastContext.Provider>
+  );
+};
+
+const useToastContext = () => {
+  const ctx = useContext(ToastContext);
+  if (!ctx) {
+    throw new Error("ToastProviderが存在しません");
+  }
+  return ctx;
+};
+
+export const ToastAction: React.FC<ComponentPropsWithoutRef<"button">> = ({
+  children,
+  ...props
+}) => {
+  return (
+    <button
+      className="h-7 rounded border border-neutral-300 bg-neutral-100 px-2 text-xs transition-colors hover:bg-black/5"
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
+
+export const useToast = () => {
+  const [_, setToasts] = useToastContext();
+
+  const toast = useCallback(
+    (input: CreateToastInput) => {
+      const id = crypto.randomUUID();
+      setToasts((toasts) => [...toasts, { id, ...input }]);
+
+      const close = () => {
+        setToasts((toasts) => toasts.filter((toast) => toast.id !== id));
+      };
+
+      return { close };
+    },
+    [setToasts],
+  );
+
+  return { toast };
+};
