d6f457ba970c32e27ba4e94d039d809405cb93f7
hwld <0701hw@gmail.com>
2024-05-10 15:14:41 +0900

    event-popoverを実装

---
+++ src/app/calendar/event-popover.tsx
@@ -0,0 +1,162 @@
+import {
+  FloatingOverlay,
+  FloatingPortal,
+  Placement,
+  flip,
+  offset,
+  shift,
+  useDismiss,
+  useFloating,
+  useInteractions,
+} from "@floating-ui/react";
+import { Slot } from "@radix-ui/react-slot";
+import { ComponentPropsWithoutRef, ReactNode, useMemo } from "react";
+import { Event } from "./mocks/event-store";
+import { AnimatePresence, motion } from "framer-motion";
+import { TbClockHour5, TbPencilMinus, TbTrash, TbX } from "react-icons/tb";
+import { IconType } from "react-icons/lib";
+import { format, isSameDay, isSameYear } from "date-fns";
+
+type Props = {
+  event: Event;
+  children: ReactNode;
+  isOpen: boolean;
+  onChangeOpen: (open: boolean) => void;
+  placement?: Placement;
+};
+
+export const EventPopover: React.FC<Props> = ({
+  event,
+  children,
+  isOpen,
+  onChangeOpen,
+  placement = "bottom",
+}) => {
+  const { refs, floatingStyles, context } = useFloating({
+    open: isOpen,
+    onOpenChange: onChangeOpen,
+    placement,
+    middleware: [offset(8), flip(), shift({ padding: 8 })],
+  });
+
+  const dismiss = useDismiss(context);
+
+  const { getReferenceProps, getFloatingProps } = useInteractions([dismiss]);
+
+  return (
+    <>
+      <Slot ref={refs.setReference} {...getReferenceProps()}>
+        {children}
+      </Slot>
+      <AnimatePresence>
+        {isOpen && (
+          <FloatingPortal>
+            <FloatingOverlay
+              lockScroll
+              className="z-[100]"
+              onMouseDown={(e) => e.stopPropagation()}
+            >
+              <div
+                ref={refs.setFloating}
+                style={floatingStyles}
+                {...getFloatingProps()}
+              >
+                <motion.div
+                  initial={{ opacity: 0, y: -8 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: -8 }}
+                  transition={{ duration: 0.1 }}
+                  className="w-[320px] space-y-2 rounded-lg border border-neutral-300 bg-neutral-100 text-neutral-700 shadow"
+                >
+                  <div className="flex items-center justify-between px-2 pt-2">
+                    <div className="text-xs text-neutral-400">イベント詳細</div>
+                    <div className="flex">
+                      <Button icon={TbPencilMinus} />
+                      <Button icon={TbTrash} />
+                      <Button icon={TbX} onClick={() => onChangeOpen(false)} />
+                    </div>
+                  </div>
+                  <div className="space-y-2 px-4 pb-4">
+                    <div className="font-bold">{event.title}</div>
+                    <div className="flex items-center gap-1 text-sm">
+                      <TbClockHour5 size={18} className="shrink-0" />
+                      <EventPeriod event={event} />
+                    </div>
+                  </div>
+                </motion.div>
+              </div>
+            </FloatingOverlay>
+          </FloatingPortal>
+        )}
+      </AnimatePresence>
+    </>
+  );
+};
+
+const EventPeriod: React.FC<{ event: Event }> = ({ event }) => {
+  const content = useMemo(() => {
+    const { start, end } = event;
+
+    const now = Date.now();
+
+    // どちらかが今年ではなければどちらの年月も表示する
+    const showYear = !(isSameYear(now, start) && isSameYear(now, end));
+
+    const startYearOrNone = showYear ? format(start, "yyyy年") : "";
+    const startDate = format(start, "M月d日");
+    const startTime = format(event.start, "HH:mm");
+
+    const endYearOrNone = showYear ? format(end, "yyyy年") : "";
+    const endDate = format(end, "M月d日");
+    const endTime = format(end, "HH:mm");
+
+    const wave = <span className="mx-2">~</span>;
+
+    if (event.allDay) {
+      return (
+        <>
+          {startYearOrNone}
+          {startDate}
+          {wave}
+          {endYearOrNone}
+          {endDate}
+        </>
+      );
+    } else if (isSameDay(start, end)) {
+      return (
+        <>
+          {startYearOrNone}
+          {startDate}・{startTime}
+          {wave}
+          {endTime}
+        </>
+      );
+    } else {
+      return (
+        <>
+          {startYearOrNone}
+          {startDate}・{startTime}
+          {wave}
+          {showYear ? <br /> : ""}
+          {endYearOrNone}
+          {endDate}・{endTime}
+        </>
+      );
+    }
+  }, [event]);
+
+  return <div className="tabular-nums">{content}</div>;
+};
+
+const Button: React.FC<
+  { icon: IconType } & ComponentPropsWithoutRef<"button">
+> = ({ icon: Icon, ...props }) => {
+  return (
+    <button
+      className="grid size-7 place-items-center rounded text-neutral-600 transition-colors hover:bg-black/10"
+      {...props}
+    >
+      <Icon size="60%" />
+    </button>
+  );
+};
