1e33cd7c7dd5ba6abdfcf5b17f7b2f7ae15bf65c
hwld <0701hw@gmail.com>
2024-05-16 19:23:10 +0900

    アニメーションを実行するためにselectをradix-uiではなくheadless-uiで実装してみた

--- src/app/calendar/_components/select.tsx
+++ src/app/calendar/_components/select.tsx
@@ -1,7 +1,12 @@
-import * as RxSelect from "@radix-ui/react-select";
-import { TbCheck, TbChevronDown } from "react-icons/tb";
+import {
+  Listbox,
+  ListboxButton,
+  ListboxOption,
+  ListboxOptions,
+} from "@headlessui/react";
+import { AnimatePresence, motion } from "framer-motion";
 import { Button } from "./button";
-import { forwardRef, useState } from "react";
+import { TbCheck, TbChevronDown } from "react-icons/tb";
 
 type Props<T extends string> = {
   value: T;
@@ -14,71 +19,64 @@ export const Select = <T extends string>({
   items,
   onSelect,
 }: Props<T>) => {
-  const [open, setOpen] = useState(false);
-
-  const handleSelect = (value: string) => {
-    onSelect(value as T);
-  };
+  const selectedItem = items.find((item) => item.value === value);
 
   return (
-    <RxSelect.Root
-      open={open}
-      value={value}
-      onOpenChange={setOpen}
-      onValueChange={handleSelect}
+    <Listbox
+      value={selectedItem}
+      onChange={(e) => {
+        onSelect(e.value);
+      }}
     >
-      <RxSelect.Trigger asChild>
-        <Button active={open}>
-          <div className="flex items-center gap-2">
-            <RxSelect.Value />
-            <RxSelect.Icon>
-              <TbChevronDown />
-            </RxSelect.Icon>
-          </div>
-        </Button>
-      </RxSelect.Trigger>
-
-      <RxSelect.Portal>
-        <RxSelect.Content
-          position="popper"
-          side="bottom"
-          sideOffset={4}
-          className="min-w-[150px] rounded border border-neutral-300 bg-neutral-50 p-[2px] text-sm text-neutral-700 shadow data-[state=open]:animate-in data-[state=open]:slide-in-from-top-1"
-        >
-          <RxSelect.Viewport>
-            {items.map((item) => {
-              return (
-                <SelectItem
-                  key={item.value}
-                  value={item.value}
-                  option={item.option}
+      {({ open, value }) => {
+        return (
+          <>
+            <ListboxButton as={Button} active={open}>
+              <div className="flex items-center justify-between gap-1">
+                <div>{value.label}</div>
+                <TbChevronDown />
+              </div>
+            </ListboxButton>
+            <AnimatePresence>
+              {open && (
+                <ListboxOptions
+                  static
+                  as={motion.div}
+                  initial={{ opacity: 0, y: -4 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: -4 }}
+                  transition={{ duration: 0.1 }}
+                  anchor={{ to: "bottom start", gap: "4px", padding: "4px" }}
+                  className="min-w-[150px] origin-top rounded border border-neutral-300 bg-neutral-50 p-[2px] text-sm text-neutral-700 shadow focus:outline-none"
                 >
-                  {item.label}
-                </SelectItem>
-              );
-            })}
-          </RxSelect.Viewport>
-        </RxSelect.Content>
-      </RxSelect.Portal>
-    </RxSelect.Root>
+                  {items.map((item) => {
+                    return (
+                      <ListboxOption
+                        key={item.value}
+                        value={item}
+                        className="group flex h-7 cursor-pointer items-center justify-between gap-2 rounded px-2 text-sm transition-colors data-[focus]:bg-neutral-200"
+                      >
+                        <div className="flex items-center gap-1">
+                          <TbCheck
+                            className="invisible group-data-[selected]:visible"
+                            size={16}
+                          />
+                          {item.label}
+                        </div>
+                        {item.option && (
+                          <div className="text-xs text-neutral-500">
+                            {item.option}
+                          </div>
+                        )}
+                      </ListboxOption>
+                    );
+                  })}
+                </ListboxOptions>
+              )}
+            </AnimatePresence>
+          </>
+        );
+      }}
+    </Listbox>
   );
 };
-
-const SelectItem = forwardRef<
-  HTMLDivElement,
-  RxSelect.SelectItemProps & { option?: string }
->(function SelectItem({ children, option, ...props }, ref) {
-  return (
-    <RxSelect.SelectItem
-      ref={ref}
-      {...props}
-      className="relative flex h-7 cursor-pointer items-center justify-between gap-2 rounded pl-[25px] pr-2 transition-colors data-[highlighted]:bg-neutral-200 data-[highlighted]:outline-none"
-    >
-      <RxSelect.ItemIndicator className="absolute left-1 top-1/2 -translate-y-1/2 text-[17px]">
-        <TbCheck />
-      </RxSelect.ItemIndicator>
-      <RxSelect.ItemText>{children}</RxSelect.ItemText>
-      {option && <div className="text-xs text-neutral-500">{option}</div>}
-    </RxSelect.SelectItem>
-  );
-});
