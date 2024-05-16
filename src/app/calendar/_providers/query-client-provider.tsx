b9feea4165faa4531169274883bd277be946b5cb
hwld <0701hw@gmail.com>
2024-05-16 13:46:15 +0900

    データ読み込みエラーを表示させる

--- src/app/calendar/_providers/query-client-provider.tsx
+++ src/app/calendar/_providers/query-client-provider.tsx
@@ -1,14 +1,30 @@
 import {
+  QueryCache,
   QueryClient,
   QueryClientProvider as _QueryClientProvider,
 } from "@tanstack/react-query";
 import { PropsWithChildren, useState } from "react";
+import { useToast } from "../_components/toast";
 
 export const QueryClientProvider: React.FC<PropsWithChildren> = ({
   children,
 }) => {
+  const { toast } = useToast();
+
   const [queryClient] = useState(() => {
     return new QueryClient({
+      queryCache: new QueryCache({
+        onError: () => {
+          toast.error({
+            title: "データの読み込みに失敗しました",
+            description: "画面を更新して、もう一度試してみてください",
+            actionText: "画面を更新",
+            action: () => {
+              window.location.reload();
+            },
+          });
+        },
+      }),
       defaultOptions: {
         queries: { retry: 1 },
         mutations: {
