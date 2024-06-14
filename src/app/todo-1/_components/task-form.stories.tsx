5df2bd001ddbc449796677a249278fc1404f0259
hwld <0701hw@gmail.com>
2024-06-14 10:42:33 +0900

    storybookでタスク作成APIが呼び出されたかをより正確にテストする

--- src/app/todo-1/_components/task-form.stories.tsx
+++ src/app/todo-1/_components/task-form.stories.tsx
@@ -1,13 +1,26 @@
 import { Meta, StoryObj } from "@storybook/react";
 import { TaskForm } from "./task-form";
 import { defaultStoryMeta } from "../story-meta";
-import { expect, userEvent, waitFor, within } from "@storybook/test";
+import { expect, fn, userEvent, waitFor, within } from "@storybook/test";
+import { Todo1API, createTaskInputSchema } from "../_mocks/api";
+import { http } from "msw";
+
+const createTaskMock = fn();
 
 const meta = {
   ...defaultStoryMeta,
+  parameters: {
+    msw: {
+      handlers: [
+        http.post(Todo1API.tasks(), async ({ request }) => {
+          const input = createTaskInputSchema.parse(await request.json());
+          createTaskMock(input.title);
+        }),
+      ],
+    },
+  },
   title: "Todo1/TaskForm",
   component: TaskForm,
-  args: { onSubmit: () => {} },
 } satisfies Meta<typeof TaskForm>;
 
 export default meta;
@@ -15,12 +28,19 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   play: async ({ canvasElement }) => {
+    // canvasElementを直接使うと、portalが見えない
+    // https://github.com/storybookjs/storybook/issues/16971
     const canvas = within(canvasElement.parentElement!);
     const titleInput = canvas.getByRole("textbox");
+    const title = "たすく";
 
-    await userEvent.type(titleInput, "title{enter}", { delay: 50 });
+    await userEvent.type(titleInput, `${title}{enter}`, { delay: 50 });
 
-    await waitFor(() => expect(titleInput).toHaveValue(""));
+    await waitFor(async () => {
+      await expect(createTaskMock).toHaveBeenCalledTimes(1);
+      await expect(createTaskMock).toHaveBeenCalledWith(title);
+      await expect(titleInput).toHaveValue("");
+    });
   },
 };
 
@@ -31,14 +51,15 @@ export const NoTitleError: Story = {
 
     await userEvent.type(titleInput, "{enter}");
 
-    await waitFor(() => expect(canvas.getByRole("alert")).toBeInTheDocument());
+    await waitFor(async () => {
+      await expect(createTaskMock).not.toHaveBeenCalled();
+      await expect(canvas.getByRole("alert")).toBeInTheDocument();
+    });
   },
 };
 
 export const MaxLengthError: Story = {
   play: async ({ canvasElement }) => {
-    // canvasElementを直接使うと、portalが見えない
-    // https://github.com/storybookjs/storybook/issues/16971
     const canvas = within(canvasElement.parentElement!);
     const titleInput = canvas.getByRole("textbox");
 
@@ -46,6 +67,9 @@ export const MaxLengthError: Story = {
     await userEvent.paste(`${"a".repeat(200)}`);
     await userEvent.type(titleInput, "{enter}");
 
-    await waitFor(() => expect(canvas.getByRole("alert")).toBeInTheDocument());
+    await waitFor(async () => {
+      await expect(createTaskMock).not.toHaveBeenCalled();
+      await expect(canvas.getByRole("alert")).toBeInTheDocument();
+    });
   },
 };
