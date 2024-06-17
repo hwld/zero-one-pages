8e5039c78a540e5a00d54d84d58367aa1f8d1d79
hwld <0701hw@gmail.com>
2024-06-17 22:00:05 +0900

    todo-1の詳細ページでTaskDetailSheetを使用していなかったので、使用するように変更した

--- src/app/todo-1/_components/task-detail-sheet/task-detail-sheet.tsx
+++ src/app/todo-1/_components/task-detail-sheet/task-detail-sheet.tsx
@@ -5,94 +5,99 @@ import { TaskStatusBadge } from "../task-status-badge";
 import { TaskDescriptionForm } from "./task-description-form";
 import { Task } from "../../_mocks/task-store";
 import { useUpdateTask } from "../../_queries/use-update-task";
+import { forwardRef } from "react";
 
-export const TaskDetailSheet: React.FC<{
+type Props = {
   task: Task;
   isOpen: boolean;
   onOpenChange: (open: boolean) => void;
-}> = ({ task, isOpen, onOpenChange }) => {
-  const updateTaskMutation = useUpdateTask();
+};
 
-  return (
-    <RadixDialog.Root open={isOpen} onOpenChange={onOpenChange}>
-      <AnimatePresence>
-        {isOpen && (
-          <RadixDialog.Portal forceMount>
-            <RadixDialog.Overlay forceMount asChild>
-              <motion.div
-                className="fixed inset-0 bg-black/10"
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-              />
-            </RadixDialog.Overlay>
+export const TaskDetailSheet = forwardRef<HTMLDivElement, Props>(
+  function TaskDetailSheet({ task, isOpen, onOpenChange }, ref) {
+    const updateTaskMutation = useUpdateTask();
 
-            <RadixDialog.Content forceMount asChild>
-              <motion.div
-                className="fixed bottom-0 right-0 top-0 z-10 w-[450px] max-w-full p-3"
-                tabIndex={undefined}
-                initial={{ x: 20, opacity: 0 }}
-                animate={{ x: 0, opacity: 1 }}
-                exit={{ x: 20, opacity: 0 }}
-              >
-                <div className="relative flex h-full w-full flex-col gap-6 overflow-auto rounded-lg border-neutral-300 bg-neutral-100 p-6 text-neutral-700 [&_*]:outline-neutral-900">
-                  <RadixDialog.Close asChild>
-                    <button
-                      className="absolute right-3 top-3 rounded p-1 text-neutral-700 transition-colors hover:bg-black/5"
-                      aria-label="シートを閉じる"
-                    >
-                      <XIcon />
-                    </button>
-                  </RadixDialog.Close>
-                  <div className="space-y-1">
-                    <div className="text-xs text-neutral-500">title</div>
-                    <div className="text-2xl font-bold">{task.title}</div>
-                    <div className="text-xs text-neutral-500">
-                      ID: {task.id}
+    return (
+      <RadixDialog.Root open={isOpen} onOpenChange={onOpenChange}>
+        <AnimatePresence>
+          {isOpen && (
+            <RadixDialog.Portal forceMount>
+              <RadixDialog.Overlay forceMount asChild>
+                <motion.div
+                  className="fixed inset-0 bg-black/10"
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                />
+              </RadixDialog.Overlay>
+
+              <RadixDialog.Content forceMount asChild>
+                <motion.div
+                  ref={ref}
+                  className="fixed bottom-0 right-0 top-0 z-10 w-[450px] max-w-full p-3"
+                  tabIndex={undefined}
+                  initial={{ x: 20, opacity: 0 }}
+                  animate={{ x: 0, opacity: 1 }}
+                >
+                  <div className="relative flex h-full w-full flex-col gap-6 overflow-auto rounded-lg border-neutral-300 bg-neutral-100 p-6 text-neutral-700 [&_*]:outline-neutral-900">
+                    <RadixDialog.Close asChild>
+                      <button
+                        className="absolute right-3 top-3 rounded p-1 text-neutral-700 transition-colors hover:bg-black/5"
+                        aria-label="シートを閉じる"
+                      >
+                        <XIcon />
+                      </button>
+                    </RadixDialog.Close>
+                    <div className="space-y-1">
+                      <div className="text-xs text-neutral-500">title</div>
+                      <div className="text-2xl font-bold">{task.title}</div>
+                      <div className="text-xs text-neutral-500">
+                        ID: {task.id}
+                      </div>
                     </div>
-                  </div>
-                  <div className="space-y-2">
-                    <div className="flex items-center gap-1 text-sm text-neutral-500">
-                      <ActivityIcon size={18} />
-                      <div>状態</div>
+                    <div className="space-y-2">
+                      <div className="flex items-center gap-1 text-sm text-neutral-500">
+                        <ActivityIcon size={18} />
+                        <div>状態</div>
+                      </div>
+                      <div className="ml-2">
+                        <TaskStatusBadge
+                          done={task.done}
+                          onChangeDone={() => {
+                            updateTaskMutation.mutate({
+                              ...task,
+                              done: !task.done,
+                            });
+                          }}
+                        />
+                      </div>
                     </div>
-                    <div className="ml-2">
-                      <TaskStatusBadge
-                        done={task.done}
-                        onChangeDone={() => {
+                    <div className="space-y-2">
+                      <label
+                        className="flex items-center gap-1 text-sm text-neutral-500"
+                        htmlFor="description"
+                      >
+                        <TextIcon size={18} />
+                        <div>説明</div>
+                      </label>
+                      <TaskDescriptionForm
+                        id="description"
+                        defaultDescription={task.description}
+                        onChangeDescription={(desc) => {
                           updateTaskMutation.mutate({
                             ...task,
-                            done: !task.done,
+                            description: desc,
                           });
                         }}
                       />
                     </div>
                   </div>
-                  <div className="space-y-2">
-                    <label
-                      className="flex items-center gap-1 text-sm text-neutral-500"
-                      htmlFor="description"
-                    >
-                      <TextIcon size={18} />
-                      <div>説明</div>
-                    </label>
-                    <TaskDescriptionForm
-                      id="description"
-                      defaultDescription={task.description}
-                      onChangeDescription={(desc) => {
-                        updateTaskMutation.mutate({
-                          ...task,
-                          description: desc,
-                        });
-                      }}
-                    />
-                  </div>
-                </div>
-              </motion.div>
-            </RadixDialog.Content>
-          </RadixDialog.Portal>
-        )}
-      </AnimatePresence>
-    </RadixDialog.Root>
-  );
-};
+                </motion.div>
+              </RadixDialog.Content>
+            </RadixDialog.Portal>
+          )}
+        </AnimatePresence>
+      </RadixDialog.Root>
+    );
+  },
+);
--- src/app/todo-1/detail/page.tsx
+++ src/app/todo-1/detail/page.tsx
@@ -1,20 +1,15 @@
 "use client";
-import { motion, useAnimate } from "framer-motion";
-import * as RadixDialog from "@radix-ui/react-dialog";
-import { ActivityIcon, TextIcon, XIcon } from "lucide-react";
+import { useAnimate } from "framer-motion";
 import { useRouter, useSearchParams } from "next/navigation";
 import { z } from "zod";
 import { useTask } from "../_queries/use-task";
-import { useUpdateTask } from "../_queries/use-update-task";
-import { TaskStatusBadge } from "../_components/task-status-badge";
-import { TaskDescriptionForm } from "../_components/task-detail-sheet/task-description-form";
+import { TaskDetailSheet } from "../_components/task-detail-sheet/task-detail-sheet";
 
 const DetailModalPage: React.FC = () => {
   const router = useRouter();
   const params = useSearchParams();
   const taskId = z.string().parse(params.get("id"));
   const { data: task } = useTask(taskId);
-  const updateTaskMutation = useUpdateTask();
 
   const [scope, animate] = useAnimate();
   const handleOpenChange = async (open: boolean) => {
@@ -32,73 +27,14 @@ const DetailModalPage: React.FC = () => {
   }
 
   return (
-    <RadixDialog.Root open onOpenChange={handleOpenChange}>
-      <RadixDialog.Portal forceMount>
-        <RadixDialog.Overlay forceMount asChild>
-          <motion.div
-            className="fixed inset-0 bg-black/10"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-          />
-        </RadixDialog.Overlay>
-
-        <RadixDialog.Content forceMount asChild>
-          <motion.div
-            ref={scope}
-            className="fixed bottom-0 right-0 top-0 z-10 w-[450px] max-w-full p-3"
-            tabIndex={undefined}
-            initial={{ x: 20, opacity: 0 }}
-            animate={{ x: 0, opacity: 1 }}
-          >
-            <div className="relative flex h-full w-full flex-col gap-6 overflow-auto rounded-lg border-neutral-300 bg-neutral-100 p-6 text-neutral-700 [&_*]:outline-neutral-900">
-              <RadixDialog.Close asChild>
-                <button className="absolute right-3 top-3 rounded p-1 text-neutral-700 transition-colors hover:bg-black/5">
-                  <XIcon />
-                </button>
-              </RadixDialog.Close>
-              <div className="space-y-1">
-                <div className="text-xs text-neutral-500">task-title</div>
-                <div className="text-2xl font-bold">{task.title}</div>
-                <div className="text-xs text-neutral-500">ID: {task.id}</div>
-              </div>
-              <div className="space-y-2">
-                <div className="flex items-center gap-1 text-sm text-neutral-500">
-                  <ActivityIcon size={18} />
-                  <div>状態</div>
-                </div>
-                <div className="ml-2">
-                  <TaskStatusBadge
-                    done={task.done}
-                    onChangeDone={() => {
-                      updateTaskMutation.mutate({
-                        ...task,
-                        done: !task.done,
-                      });
-                    }}
-                  />
-                </div>
-              </div>
-              <div className="space-y-2">
-                <div className="flex items-center gap-1 text-sm text-neutral-500">
-                  <TextIcon size={18} />
-                  <div>説明</div>
-                </div>
-                <TaskDescriptionForm
-                  defaultDescription={task.description}
-                  onChangeDescription={(desc) => {
-                    updateTaskMutation.mutate({
-                      ...task,
-                      description: desc,
-                    });
-                  }}
-                />
-              </div>
-            </div>
-          </motion.div>
-        </RadixDialog.Content>
-      </RadixDialog.Portal>
-    </RadixDialog.Root>
+    <>
+      <TaskDetailSheet
+        task={task}
+        isOpen
+        onOpenChange={handleOpenChange}
+        ref={scope}
+      />
+    </>
   );
 };
 
