6d6721c1d79a5ffac443fb856ac4d043cab0b4d8
hwld <0701hw@gmail.com>
2024-07-29 23:47:23 +0900

    SidebarListItemを実装

--- src/app/todoist/_components/sidebar.tsx
+++ src/app/todoist/_components/sidebar.tsx
@@ -1,11 +1,25 @@
 "use client";
 
-import React, { ComponentPropsWithoutRef, useMemo } from "react";
+import React, { ComponentPropsWithoutRef, ReactNode, useMemo } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import { Resizable } from "re-resizable";
 import { useRef, useState } from "react";
-import { PiBellSimple, PiSidebarSimple } from "react-icons/pi";
+import {
+  PiBellSimple,
+  PiCalendarDotsFill,
+  PiCalendarDotsLight,
+  PiCalendarFill,
+  PiCalendarLight,
+  PiCaretDown,
+  PiMagnifyingGlassLight,
+  PiSidebarSimple,
+  PiSquaresFourFill,
+  PiSquaresFourLight,
+  PiTrayFill,
+  PiTrayLight,
+} from "react-icons/pi";
 import { IconType } from "react-icons/lib";
+import clsx from "clsx";
 
 export const Sidebar: React.FC = () => {
   const resizableRef = useRef<Resizable>(null);
@@ -25,11 +39,14 @@ export const Sidebar: React.FC = () => {
     <div className="h-full w-1 transition-colors group-hover:bg-black/10 group-active:bg-black/20" />
   );
 
+  // TODO:
+  const [activeId, setActiveId] = useState(0);
+
   return (
     <motion.div className="flex" animate={{ marginLeft }}>
       <Resizable
         ref={resizableRef}
-        className="bg-neutral-200"
+        className="bg-stone-200/40"
         handleClasses={{ right: handleClass }}
         handleComponent={{ right: handle }}
         enable={{ right: true }}
@@ -37,9 +54,13 @@ export const Sidebar: React.FC = () => {
         defaultSize={{ width: 250 }}
         maxWidth={420}
       >
-        <div className="relative flex size-full justify-between p-4">
-          <div className="flex h-min w-full justify-between">
-            <div></div>
+        <div className="relative flex size-full flex-col  p-4">
+          <div className="mb-4 flex h-min w-full justify-between">
+            <button className="group flex h-8 items-center gap-2 rounded p-2 transition-colors hover:bg-black/5">
+              <div className="size-6 rounded-full bg-rose-700" />
+              <div className="font-bold">User</div>
+              <PiCaretDown className="text-stone-600 group-hover:text-stone-900" />
+            </button>
             <div className="flex items-center gap-1">
               <SidebarIconButton icon={PiBellSimple} />
               <SidebarIconButton
@@ -47,6 +68,7 @@ export const Sidebar: React.FC = () => {
                 onClick={() => {
                   setIsOpen(false);
                 }}
+                style={{ opacity: isOpen ? 1 : 0 }}
               />
             </div>
           </div>
@@ -67,6 +89,42 @@ export const Sidebar: React.FC = () => {
               </motion.div>
             )}
           </AnimatePresence>
+
+          <ul>
+            <SidebarListItem icon={PiMagnifyingGlassLight}>
+              検索
+            </SidebarListItem>
+            <SidebarListItem
+              right={3}
+              active={activeId === 0}
+              icon={activeId === 0 ? PiTrayFill : PiTrayLight}
+              onClick={() => setActiveId(0)}
+            >
+              インボックス
+            </SidebarListItem>
+            <SidebarListItem
+              right={10}
+              active={activeId === 1}
+              icon={activeId === 1 ? PiCalendarFill : PiCalendarLight}
+              onClick={() => setActiveId(1)}
+            >
+              今日
+            </SidebarListItem>
+            <SidebarListItem
+              active={activeId === 2}
+              icon={activeId === 2 ? PiCalendarDotsFill : PiCalendarDotsLight}
+              onClick={() => setActiveId(2)}
+            >
+              近日予定
+            </SidebarListItem>
+            <SidebarListItem
+              active={activeId === 3}
+              icon={activeId === 3 ? PiSquaresFourFill : PiSquaresFourLight}
+              onClick={() => setActiveId(3)}
+            >
+              フィルター & ラベル
+            </SidebarListItem>
+          </ul>
         </div>
       </Resizable>
     </motion.div>
@@ -79,9 +137,45 @@ const SidebarIconButton: React.FC<
   return (
     <button
       {...props}
-      className="grid size-8 place-items-center rounded transition-colors hover:bg-black/10"
+      className="grid size-8 place-items-center rounded text-stone-600 transition-colors hover:bg-black/5 hover:text-stone-900"
     >
       <Icon className="size-5" />
     </button>
+  );
+};
+
+const SidebarListItem: React.FC<
+  {
+    icon: IconType;
+    active?: boolean;
+    right?: ReactNode;
+  } & ComponentPropsWithoutRef<"button">
+> = ({ icon: Icon, right, active, children, ...props }) => {
+  const mutedTextClass = "text-stone-500";
+  const activeTextClass = "text-rose-700";
+
+  return (
+    <li>
+      <button
+        {...props}
+        className={clsx(
+          "group flex h-9 w-full items-center justify-between gap-2 rounded px-2 transition-colors",
+          active ? clsx("bg-rose-100", activeTextClass) : "hover:bg-black/5",
+        )}
+      >
+        <div className="flex min-w-0 items-center gap-1">
+          <Icon
+            className={clsx(
+              "size-6 shrink-0 text-stone-500",
+              active ? activeTextClass : mutedTextClass,
+            )}
+          />
+          <div className="truncate">{children}</div>
+        </div>
+        <div className={clsx(active ? activeTextClass : mutedTextClass)}>
+          {right}
+        </div>
+      </button>
+    </li>
   );
 };
--- src/app/todoist/page.tsx
+++ src/app/todoist/page.tsx
@@ -3,7 +3,7 @@ import { Sidebar } from "./_components/sidebar";
 
 const Page: React.FC = () => {
   return (
-    <div className="flex h-[100dvh] bg-neutral-100 text-neutral-700">
+    <div className="flex h-[100dvh] bg-stone-50 text-sm text-neutral-700">
       <Sidebar />
       <div></div>
     </div>
