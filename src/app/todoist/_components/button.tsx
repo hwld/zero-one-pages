c40e0883de0fc823a555e1b81f5bbebdd67823e7
hwld <0701hw@gmail.com>
2024-08-14 16:33:01 +0900

    ボタンがフォーカスされているときのスタイルを変更

--- src/app/todoist/_components/button.tsx
+++ src/app/todoist/_components/button.tsx
@@ -12,18 +12,20 @@ export const Button: React.FC<Props> = ({
   ...props
 }) => {
   const colorClass = {
-    primary: "bg-rose-700 text-stone-100 hover:bg-rose-800",
-    secondary: "bg-stone-200 text-stone-700 hover:bg-stone-300",
+    primary:
+      "bg-rose-700 text-stone-100 hover:bg-rose-800 focus-visible:outline-none focus-visible:ring-2 ring-rose-400",
+    secondary:
+      "bg-stone-200 text-stone-700 hover:bg-stone-300 focus-visible:outline-none focus-visible:ring-2 ring-stone-500",
   };
   const sizeClass = {
-    md: "h-8 px-3 text-sm font-medium",
+    md: "h-8 px-3 text-sm font-medium min-w-16",
     sm: "h-6 text-xs px-2",
   };
 
   return (
     <button
       className={clsx(
-        "select-none rounded transition-all active:scale-95",
+        "select-none rounded ring-offset-2 transition-all focus-visible:outline-none focus-visible:ring-2 active:scale-95",
         sizeClass[size],
         colorClass[color],
       )}
