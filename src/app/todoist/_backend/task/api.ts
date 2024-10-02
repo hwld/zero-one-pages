6569b90389b2b9adb22e54d6e8b414dc1ea00027
hwld <0701hw@gmail.com>
2024-10-02 23:40:43 +0900

    タスク取得APIを実装

--- src/app/todoist/_backend/task/api.ts
+++ src/app/todoist/_backend/task/api.ts
@@ -9,9 +9,17 @@ import { TodoistAPI } from "../routes";
 import { fetcher } from "../../../../lib/fetcher";
 import { taskRepository } from "./repository";
 import { z } from "zod";
-import { taskSchema } from "./model";
+import { taskSchema, type Task } from "./model";
 
-export const fetchTasks = async () => {
+export const fetchTask = async (id: string): Promise<Task> => {
+  const res = await fetcher.get(TodoistAPI.task(id));
+  const json = await res.json();
+  const task = taskSchema.parse(json);
+
+  return task;
+};
+
+export const fetchTasks = async (): Promise<Task[]> => {
   const res = await fetcher.get(TodoistAPI.tasks());
   const json = await res.json();
   const tasks = z.array(taskSchema).parse(json);
@@ -19,25 +27,25 @@ export const fetchTasks = async () => {
   return tasks;
 };
 
-export const createTask = async (input: TaskFormData) => {
+export const createTask = async (input: TaskFormData): Promise<void> => {
   await fetcher.post(TodoistAPI.tasks(), { body: input });
 };
 
 export const updateTask = async ({
   id,
   ...input
-}: TaskFormData & { id: string }) => {
+}: TaskFormData & { id: string }): Promise<void> => {
   await fetcher.patch(TodoistAPI.task(id), { body: input });
 };
 
 export const updateTaskDone = async ({
   id,
   ...body
-}: UpdateTaskDoneInput & { id: string }) => {
+}: UpdateTaskDoneInput & { id: string }): Promise<void> => {
   await fetcher.patch(TodoistAPI.updateTaskDone(id), { body });
 };
 
-export const deleteTask = async (id: string) => {
+export const deleteTask = async (id: string): Promise<void> => {
   await fetcher.delete(TodoistAPI.task(id));
 };
 
@@ -60,6 +68,18 @@ export const taskApiHandlers = [
     });
 
     return HttpResponse.json({});
+  }),
+
+  http.get(TodoistAPI.task(), async ({ params }) => {
+    await delay();
+    const taskId = z.string().parse(params.id);
+
+    const task = taskRepository.get(taskId);
+    if (!task) {
+      return new HttpResponse(null, { status: 404 });
+    }
+
+    return HttpResponse.json(task);
   }),
 
   http.patch(TodoistAPI.task(), async ({ request, params }) => {
