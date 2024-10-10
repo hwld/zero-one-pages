5a77aa304b66c6de44798fa52aa3f0d7e29356e6
hwld <0701hw@gmail.com>
2024-10-10 11:15:34 +0900

    searchParamsが変わったときにエラーページがresetされるようにした

--- src/app/todoist/error.tsx
+++ src/app/todoist/error.tsx
@@ -1,9 +1,27 @@
 "use client";
 
-const ErrorPage: React.FC = () => {
+import { useEffect, useRef } from "react";
+import { ButtonLink } from "./_components/button";
+import { useSearchParams } from "next/navigation";
+import { Routes } from "./routes";
+
+const ErrorPage: React.FC<{ reset: () => void }> = ({ reset }) => {
+  const searchParams = useSearchParams().toString();
+
+  const prevSearchParams = useRef("");
+  useEffect(() => {
+    if (prevSearchParams.current !== searchParams) {
+      reset();
+    }
+    prevSearchParams.current = searchParams;
+  }, [reset, searchParams]);
+
   return (
-    <div className="grid size-full place-items-center">
+    <div className="grid size-full place-content-center place-items-center gap-2">
       エラーが発生しました
+      <div>
+        <ButtonLink href={Routes.inbox()}>ホームに戻る</ButtonLink>
+      </div>
     </div>
   );
 };
